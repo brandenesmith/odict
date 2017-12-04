bca535b90735c1bc7bf7abe0357d73b94de0332b
Branden Smith <smith.brandene@gmail.com>
2017-12-03 23:44:03 -0600

    Add crude implementation of dictionary command line tool.
    Added the basic functionality for the command line tool. This includes
    requesting the word from the api, and deserializing the response. Also
    the api id, and key are not stored with the repository but are actually
    environment variables, this information will need to be added to the
    README.md later.

---
+++ dictResponse.go
@@ -0,0 +1,47 @@
+package main
+
+// DictResponse is the response expected from the call to the oxford api.
+type DictResponse struct {
+	Results []DictResult `json:"results"`
+}
+
+// DictResult is a query result from the api.
+type DictResult struct {
+	Id             string         `json:"id"`
+	Language       string         `json:"language"`
+	LexicalEntries []LexicalEntry `json:"lexicalEntries"`
+}
+
+// LexicalEntry is the entry for the specified word resolving to a slice of Entry objects.
+type LexicalEntry struct {
+	Entries []Entry `json:"entries"`
+}
+
+// Entry is the actual dictionary entry for the word.
+type Entry struct {
+	Etymologies         []string             `json:"etymologies"`
+	GrammaticalFeatures []GrammaticalFeature `json:"grammaticalFeatures"`
+	HomographNumber     string               `json:"homographNumber"`
+	Senses              []Sense              `json:"senses"`
+	Language            string               `json:"language"`
+	LexicalCategory     string               `json:"lexicalCategory"`
+}
+
+// GrammaticalFeature is the actual grammatical features of the word.
+type GrammaticalFeature struct {
+	Text string `json:"text"`
+	Type string `json:"type"`
+}
+
+// Sense is the slice of definitions, examples and other definitions.
+type Sense struct {
+	Definitions []string  `json:"definitions"`
+	Example     []Example `json:"examples"`
+	Domains     []string  `json:"domains"`
+	Subsenses   []Sense   `json:"subsenses"`
+}
+
+// Example is a na example usage of the word.
+type Example struct {
+	Text string `json:"text"`
+}
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"encoding/json"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Printf("Error: You must provide the word whose definition you wish to retrieve.")
+		os.Exit(1)
+	}
+
+	url := fmt.Sprintf("https://od-api.oxforddictionaries.com/api/v1/entries/en/%s", os.Args[1])
+
+	// Create the request.
+	client := &http.Client{}
+	request, err := http.NewRequest(
+		http.MethodGet,
+		url,
+		nil,
+	)
+
+	// Add headers for api id and api key.
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("app_id", os.Getenv("ODICTAPIID"))
+	request.Header.Add("app_key", os.Getenv("ODICTAPIKEY"))
+
+	// Make the request.
+	resp, err := client.Do(request)
+
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+
+	dictResp := &DictResponse{}
+
+	json.Unmarshal(bytes, dictResp)
+
+	str := `
+===================================================
+
+| | | |     +                |
+|      |         | | |   | | | | |
+|       |   |   |            |
+|      |    |   |            |
+| | | |     |    | | |       | | |
+
+===================================================
+
+Definition:
+	%s
+`
+
+	fmt.Printf(str, dictResp.Results[0].LexicalEntries[0].Entries[0].Senses[0].Definitions[0])
+}
